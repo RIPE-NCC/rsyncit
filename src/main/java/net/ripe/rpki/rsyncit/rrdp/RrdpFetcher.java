92a09c02e6907ff3d0ddbef10dbbf96b31233dbe
Mikhail Puzanov <misha.puzanov@gmail.com>
2023-08-11 21:38:15 +0200

    Add logging of parsing errors

--- src/main/java/net/ripe/rpki/rsyncit/rrdp/RrdpFetcher.java
+++ src/main/java/net/ripe/rpki/rsyncit/rrdp/RrdpFetcher.java
@@ -3,6 +3,7 @@ package net.ripe.rpki.rsyncit.rrdp;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.ripe.rpki.commons.crypto.cms.RpkiSignedObject;
+import net.ripe.rpki.commons.crypto.cms.RpkiSignedObjectParser;
 import net.ripe.rpki.commons.crypto.cms.aspa.AspaCmsParser;
 import net.ripe.rpki.commons.crypto.cms.ghostbuster.GhostbustersCmsParser;
 import net.ripe.rpki.commons.crypto.cms.manifest.ManifestCmsParser;
@@ -43,7 +44,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -281,21 +281,6 @@ public class RrdpFetcher {
         final RepositoryObjectType objectType = RepositoryObjectType.parse(objectUri);
         try {
             return switch (objectType) {
-                case Manifest -> {
-                    ManifestCmsParser manifestCmsParser = new ManifestCmsParser();
-                    manifestCmsParser.parse(ValidationResult.withLocation(objectUri), decoded);
-                    yield extractSigningTime(manifestCmsParser.getManifestCms());
-                }
-                case Aspa -> {
-                    var aspaCmsParser = new AspaCmsParser();
-                    aspaCmsParser.parse(ValidationResult.withLocation(objectUri), decoded);
-                    yield extractSigningTime(aspaCmsParser.getAspa());
-                }
-                case Roa -> {
-                    RoaCmsParser roaCmsParser = new RoaCmsParser();
-                    roaCmsParser.parse(ValidationResult.withLocation(objectUri), decoded);
-                    yield extractSigningTime(roaCmsParser.getRoaCms());
-                }
                 case Certificate -> {
                     X509ResourceCertificateParser x509CertificateParser = new X509ResourceCertificateParser();
                     x509CertificateParser.parse(ValidationResult.withLocation(objectUri), decoded);
@@ -303,19 +288,39 @@ public class RrdpFetcher {
                     yield Instant.ofEpochMilli(cert.getNotBefore().getTime());
                 }
                 case Crl -> {
-                    final X509Crl x509Crl = X509Crl.parseDerEncoded(decoded, ValidationResult.withLocation(objectUri));
-                    final var crl = x509Crl.getCrl();
-                    yield Instant.ofEpochMilli(crl.getThisUpdate().getTime());
+                    final ValidationResult result = ValidationResult.withLocation(objectUri);
+                    final X509Crl x509Crl = X509Crl.parseDerEncoded(decoded, result);
+                    checkResult(objectUri, result);
+                    yield Instant.ofEpochMilli(x509Crl.getCrl().getThisUpdate().getTime());
                 }
-                case Gbr -> {
-                    GhostbustersCmsParser ghostbustersCmsParser = new GhostbustersCmsParser();
-                    ghostbustersCmsParser.parse(ValidationResult.withLocation(objectUri), decoded);
-                    yield extractSigningTime(ghostbustersCmsParser.getGhostbustersCms());
-                }
-                case Unknown -> lastModified;
+                case Manifest ->
+                    extractSigningTime(tryParse(new ManifestCmsParser(), objectUri, decoded).getManifestCms());
+                case Aspa ->
+                    extractSigningTime(tryParse(new AspaCmsParser(), objectUri, decoded).getAspa());
+                case Roa ->
+                    extractSigningTime(tryParse(new RoaCmsParser(), objectUri, decoded).getRoaCms());
+                case Gbr ->
+                    extractSigningTime(tryParse(new GhostbustersCmsParser(), objectUri, decoded).getGhostbustersCms());
+                case Unknown ->
+                    lastModified;
             };
         } catch (Exception e) {
+            var encoder = Base64.getEncoder();
+            log.error("Could not parse the object url = {}, body = {} :", objectUri, encoder.encodeToString(decoded), e);
             return lastModified;
+        }
+    }
+
+    private static <T extends RpkiSignedObjectParser> T tryParse(T parser, final String objectUri, final byte[] decoded) {
+        final ValidationResult result = ValidationResult.withLocation(objectUri);
+        parser.parse(result, decoded);
+        checkResult(objectUri, result);
+        return parser;
+    }
+
+    private static void checkResult(String objectUri, ValidationResult result) {
+        if (result.hasFailures()) {
+            throw new RuntimeException(String.format("Object %s, error %s", objectUri, result.getFailuresForAllLocations()));
         }
     }
 
